65b28f6962aff96e433955753f22e5fb578e849a
Darshan Poudel <pranishpoudel10@gmail.com>
2023-08-15 09:01:14 +0545

    Made Secure

--- server/Routers/authApi.js
+++ server/Routers/authApi.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import * as authController from '../controllers/authControllers.js';
+import * as authController from "../controllers/authControllers.js";
 
-const saltRounds = 5;
 const router = Router();
 
-router.post('/register',authController.register);
+router.post("/register", authController.register);
 
-router.post('/login',authController.login);
+router.post("/login", authController.login);
 
 export default router;
--- server/config/passport.js
+++ server/config/passport.js
@@ -1,38 +1,28 @@
-
-import pkg from 'passport-jwt';
+import pkg from "passport-jwt";
 const JwtStrategy = pkg.Strategy;
 const ExtractJwt = pkg.ExtractJwt;
-import User from '../models/User.js';
-import * as dotenv from 'dotenv';
+import User from "../models/User.js";
+import * as dotenv from "dotenv";
 dotenv.config();
 
-let opts = {}
+let opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.JWT_SECRET;
 
-
-export default (passport)=>{
-    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {  
-        User.findOne({_id: jwt_payload.id}).then((user)=>{
-            if(user){
-                return done(null,user);
-            } else{
-                return done(null,false);
-            }
-        }).catch((err)=>{
-            return done(err,false);
+export default (passport) => {
+  passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+      User.findOne({ _id: jwt_payload.id })
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
         })
-    // passport.use(new JwtStrategy(opts, function(jwt_payload, done) {     
-    //     User.findOne({id: jwt_payload.sub}, function(err, user) {
-    //         if (err) {
-    //             return done(err, false);
-    //         }
-    //         if (user) {
-    //             return done(null, user);
-    //         } else {
-    //             return done(null, false);
-    //             // or you could create a new account
-    //         }
-    //     });
-    }));
-}
+        .catch((err) => {
+          return done(err, false);
+        });
+    })
+  );
+};
